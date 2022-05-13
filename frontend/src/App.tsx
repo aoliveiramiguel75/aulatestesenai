07216a1fcac41ec4ce393a20544b27ca1f515a91
miguel alberto <aoliveiramiguel75@gmail.com>
2022-05-13 18:53:22 -0300

    Criação dos graficos com dados estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import Footer from "components/Footer";
+import GraficoDeBarras from "components/GraficoDeBarras";
+import GraficoDeRosca from "components/GraficoDeRosca";
 import NavBar from "components/NavBar";
 import TabelaDedados from "components/TabelaDeDados";
 
@@ -10,6 +12,26 @@ function App() {
       <NavBar />
 
       <div className="container">
+        <h1 className="text-primary py-3"> Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary"> Ticket médio </h5>
+            <GraficoDeBarras />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary"> Ticket médio </h5>
+            <GraficoDeRosca />
+          </div>          
+
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary"> Todas as Vendas (tabela)</h2>
+
+        </div>
+
         <TabelaDedados />
       </div>
 
